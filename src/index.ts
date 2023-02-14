910d0fabccc0fa87d70d24469932e2d920cac0e3
Madowwl <emma.arlanda@viacesi.fr>
2023-02-14 11:38:21 +0100

    feat: prepare config and seed data

---
+++ src/index.ts
@@ -0,0 +1,77 @@
+console.log('this is index JS');
+
+import { Header } from './app/header';
+import { Footer } from './app/footer';
+import '../src/style.css';
+import './styles/scss/main.scss';
+import { Stock } from './app/core/models/classes/stock';
+import { Article } from './app/core/models/classes/article';
+
+let header = new Header();
+
+let firstHeading = header.getFirstHeading();
+
+console.log(firstHeading);
+
+let footer = new Footer();
+
+let footerText = footer.getFooterText();
+
+//Create list of articles
+const articles: Article[] = [
+    {
+        id: 1,
+        title: 'Article 1',
+        content: 'Content 1',
+    },
+    {
+        id: 2,
+        title: 'Article 2',
+        content: 'Content 2',
+    },    
+    {
+        id: 3,
+        title: 'Article 3',
+        content: 'Content 3',
+    },    
+   
+    {
+        id: 4,
+        title: 'Article 4',
+        content: 'Content 4',
+    },    
+    {
+        id: 5,
+        title: 'Article 5',
+        content: 'Content 5',
+    }, 
+]
+export const myStock: Stock = new Stock(articles);
+
+let title: HTMLElement | any = document.getElementById('title');
+title.textContent = "Hello World";
+
+let stock: HTMLElement | any = document.getElementById('stock');
+// myStock.articles.forEach((article: Article) =>
+// {
+//     const p = document.createElement('p');
+//     p.textContent = `title : ${article.title}$, content: ${article.content}`;
+//     stock.appendChild(p);
+// });
+
+myStock.articles.forEach((article) => {
+    const articleElement = document.createElement('article');
+    articleElement.innerHTML = 
+        `<h2>${article.title}</h2><p>${article.content}</p>`
+    ;
+    document.body.appendChild(articleElement);
+    });
+
+// myStock.articles.map((article: Article) =>
+// {
+// });
+
+
+console.log(footerText);
+
+
