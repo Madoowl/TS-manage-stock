b01c7e4d29206303021568ce1fd50552d3b679ce
Madowwl <emma.arlanda@viacesi.fr>
2023-02-14 10:58:17 +0100

    feat: add model
    add article model

---
+++ src/app/core/models/classes/article.ts
@@ -0,0 +1,16 @@
+export class Article
+{
+    id!: number;
+    title!: string;
+    content!: string;
+    /**
+     *
+     */
+    constructor(id: number, title: string, content: string) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+    
+}
