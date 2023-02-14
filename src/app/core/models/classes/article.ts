be67a01497053c92f4f8aa42c9be2d04ef91edc2
Madowwl <emma.arlanda@viacesi.fr>
2023-02-14 11:38:06 +0100

    feat: add model

--- src/app/core/models/classes/article.ts
+++ src/app/core/models/classes/article.ts
@@ -1,8 +1,8 @@
 export class Article
 {
-    id!: number;
-    title!: string;
-    content!: string;
+    id: number;
+    title: string;
+    content: string;
     /**
      *
      */
---
+++ src/app/core/models/classes/stock.ts
@@ -0,0 +1,17 @@
+import { Article } from "./article";
+
+export class Stock {
+    public articles: Article[];
+    //Create stock as an array of articles + quantity ?
+    /**
+     *
+     */
+    constructor(articles: Article[]) {
+        this.articles = [];
+    }
+    
+    public toString(): string
+    { 
+        return this.articles.toString();
+    }
+}
